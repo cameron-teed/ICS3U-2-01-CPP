f4838d94db2fabbda078377e8b16c465773a8935
Ubuntu <ubuntu@ip-172-31-21-52.ec2.internal>
2019-09-22 18:43:41 +0000

    corrected errors

---
+++ area_and_permeter_circle.cpp
@@ -0,0 +1,16 @@
+// Copyright(c) 2019 Cameron Teed All rights reserved.
+//
+// Created by: Cameron Teed
+// Created on: September 2019
+// This program calculates the area and perimeter of a circle with a
+//     a 20mm radius
+
+#include <iostream>
+#include <cmath>
+
+int main() {
+    std::cout << "if a circle has a ridius of 20mm:" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "Area is " << (M_PI*pow(20.0, 2.0)) << "mm^2" << std::endl;
+    std::cout << "Perimeter " << (2*M_PI*15) << "mm" << std::endl;
+}
